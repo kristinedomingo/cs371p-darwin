bd37b20d1dd601c37a261addebdcfb441df74ade
Kristine Domingo <kristine@cs.utexas.edu>
2015-10-26 17:00:02 -0500

    Implemented begin() and end() in Darwin (close #7)

--- Darwin.c++
+++ Darwin.c++
@@ -3,6 +3,9 @@
  *         methods in "Darwin.h".
  */
 
+#ifndef Darwin_c
+#define Darwin_c
+
 // --------
 // includes
 // --------
@@ -12,3 +15,52 @@
 #include "Darwin.h"  // Darwin interfaces
 
 using namespace std;
+
+
+// --------------------
+// Darwin (constructor)
+// --------------------
+
+/**
+ * Initializes a Darwin object, consisting of an empty grid of Species
+ * and the current_turn set to 0 (no turns have passed). Also sets the
+ * beginning (_b) and end (_e) pointers.
+ * @param s, a string of space-delimited integers
+ */
+template<int height, int width>
+Darwin<height, width>::Darwin()
+{
+    num_turns = 0;
+    current_turn = 0;
+    _b = &grid[0][0];
+    _e = _b + (height * width);
+}
+
+// -----
+// begin
+// -----
+
+/**
+ * Returns an iterator (pointer) to the beginning (left corner) of
+ * the grid.
+ */
+template<int height, int width>
+Species* Darwin<height, width>::begin() const
+{
+    return _b;
+}
+
+// ---
+// end
+// ---
+
+/**
+ * Returns an iterator (pointer) to the end (right corner) of the grid.
+ */
+template<int height, int width>
+Species* Darwin<height, width>::end() const
+{
+    return _e;
+}
+
+#endif // Darwin_c
--- Darwin.h
+++ Darwin.h
@@ -18,13 +18,69 @@
 using namespace std;
 
 
+// ----------------------------
+// Creature (Class Declaration)
+// ----------------------------
+class Creature
+{
+
+};
+
+// ---------------------------
+// Species (Class Declaration)
+// ---------------------------
+class Species
+{
+
+};
+
 // --------------------------
 // Darwin (Class Declaration)
 // --------------------------
 template<int height, int width>
 class Darwin
 {
+    private:
+        Species grid[height][width]; // the grid of species
+        int num_turns;               // total number of turns for a Darwin run
+        int current_turn;            // the current turn of the Darwin run
+        Species* _b;                 // "beginning" of the board (left corner)
+        Species* _e;                 // "end" of the board (right corner)
 
+    public:
+        // --------------------
+        // Darwin (constructor)
+        // --------------------
+
+        /**
+         * Initializes a Darwin object, consisting of an empty grid of Species
+         * and the current_turn set to 0 (no turns have passed). Also sets the
+         * beginning (_b) and end (_e) pointers.
+         * @param s, a string of space-delimited integers
+         */
+        Darwin();
+
+        // -----
+        // begin
+        // -----
+
+        /**
+         * Returns an iterator (pointer) to the beginning (left corner) of
+         * the grid.
+         */
+        Species* begin() const;
+
+        // ---
+        // end
+        // ---
+
+        /**
+         * Returns an iterator (pointer) to the end (right corner) of the grid.
+         */
+        Species* end() const;
 };
+
+#include "Darwin.c++" // http://stackoverflow.com/questions/495021/why-
+                      // can-templates-only-be-implemented-in-the-header-file
 
 #endif // Darwin_h
